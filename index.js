441008f72f8207a1b403fc8be89e54989102ab1b
brunovgois <brunovgois@gmail.com>
2023-07-10 23:55:39 -0300

    grab the images and turn then into an array

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 require("dotenv/config");
 const rp = require("request-promise");
-let $ = require("cheerio");
+const cheerio = require("cheerio");
 
 const url = process.env.URL_NAME;
 
-if(typeof(cheerio) != 'function') $=require('cheerio').default
-
 rp(url)
   .then(function (html) {
-    console.log($('a img', html).length);
-    console.log($('a img', html));
+    const $ = cheerio.load(html);
+    const images = $("a img");
+
+    const imageUrls = images
+      .map(function () {
+        return $(this).attr("src");
+      })
+      .get();
+
+    console.log("Number of images:", images.length);
+    console.log("Image URLs:", imageUrls);
   })
   .catch(function (err) {
     console.error(err);
