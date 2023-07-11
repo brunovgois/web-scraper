217fe9a1478e1a49eb40d7dadbd49337f1f5496e
brunovgois <brunovgois@gmail.com>
2023-07-11 20:16:50 -0300

    write urls to text file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv/config");
 const rp = require("request-promise");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
 const url = process.env.URL_NAME;
 
@@ -17,6 +18,15 @@ rp(url)
 
     console.log("Number of images:", images.length);
     console.log("Image URLs:", imageUrls);
+
+    fs.writeFile(
+      "weeklyComps.txt",
+      imageUrls.map(url => url.slice(2)).join("\n"),
+      { flag: "w" },
+      (err) => {
+        console.error(err);
+      }
+    );
   })
   .catch(function (err) {
     console.error(err);
