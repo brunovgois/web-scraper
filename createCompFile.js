1156f808521ed92cf4fd0326d0e626ef285a505a
brunovgois <brunovgois@gmail.com>
2023-07-13 19:38:49 -0300

    refact and compose comp objects

--- createCompFile.js
+++ createCompFile.js
@@ -8,26 +8,88 @@ const url = process.env.URL_NAME;
 rp(url)
   .then(function (html) {
     const $ = cheerio.load(html);
-    const images = $("a img");
 
-    const imageUrls = images
-      .map(function () {
-        return $(this).attr("src");
-      })
-      .get();
+    const titles = getCompTitles($);
+    const images = getImages($);
+    const texts = getHowToPlay($);
 
-    console.log("Number of images:", images.length);
-    console.log("Image URLs:", imageUrls);
+    const compositions = [];
 
-    fs.writeFile(
-      "weeklyComps.txt",
-      imageUrls.map(url => url.slice(2)).join("\n"),
-      { flag: "w" },
-      (err) => {
-        console.error(err);
+    for (let i = 0; i < titles.length; i++) {
+      const obj = {
+        title: titles[i],
+        img: images[i].slice(2),
+        text: texts[i],
+      };
+      compositions.push(obj);
+    }
+
+    const jsonString = JSON.stringify(compositions, null, 2);
+
+    fs.writeFile("./weeklyComps.json", jsonString, (err) => {
+      if (err) {
+        console.log("Error writing file", err);
+      } else {
+        console.log("Successfully wrote file");
       }
-    );
+    });
   })
   .catch(function (err) {
     console.error(err);
   });
+
+function getImages($) {
+  const images = $(".wp-block-image.size-large");
+
+  const imageUrls = images
+    .map(function () {
+      if ($(this).children().prop("name") === "img") {
+        return $(this).children("img").attr("src");
+      } else {
+        return $(this).children("a").children("img").attr("src");
+      }
+    })
+    .get();
+  return imageUrls;
+}
+function getCompTitles($) {
+  const compTitles = $("h3");
+  const titles = [];
+
+  compTitles.each(function (index, element) {
+    const $element = $(element);
+    titles.push($element.text());
+  });
+
+  return titles;
+}
+
+function getHowToPlay($) {
+  const pTags = $("p");
+
+  const extractedTexts = [];
+  var shouldExtract = false;
+  var currentText = "";
+
+  pTags.each(function (index, element) {
+    var $element = $(element);
+
+    if ($element.text().includes("How to Play")) {
+      shouldExtract = true;
+      currentText = "";
+    }
+
+    if (shouldExtract) {
+      currentText += $element.text().trim() + " ";
+
+      if ($element.text().includes("Counters")) {
+        shouldExtract = false;
+        extractedTexts.push(
+          currentText.replace("Items: Core: ", "").slice(0, -9).trim()
+        );
+      }
+    }
+  });
+
+  return extractedTexts;
+}
