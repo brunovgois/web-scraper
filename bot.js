5a45b62f0b0c15375a86f34dab7fcf94a10076c6
brunovgois <brunovgois@gmail.com>
2023-07-13 19:40:20 -0300

    send messages with delay

--- bot.js
+++ bot.js
@@ -5,35 +5,38 @@ const { Telegraf } = require("telegraf");
 const fs = require("fs");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const filePath = "weeklyComps.json";
 
-bot.command("comps", (ctx) => {
+bot.command("comps", async (ctx) => {
   console.log(ctx.from);
+  try {
+    const jsonString = await fs.promises.readFile(filePath, "utf8");
+    const objects = JSON.parse(jsonString);
 
-  const filePath = "weeklyComps.txt";
-
-  // Read the image links from the text file
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading file:", err);
-      return;
+    for (const comp of objects) {
+      await sendMessageWithDelay(ctx, comp, 300);
     }
-
-    const links = data
-      .split("\n")
-      .slice(1)
-      .map((str) => str.trim());
-
-    links.forEach((link) => {
-      ctx.telegram.sendMessage(ctx.chat.id, link, {});
-    });
-
-    /*     links.forEach(async (link) => {
-      const fileUrl = await ctx.telegram.getFileLink(link);
-      ctx.replyWithPhoto({ url: fileUrl });
-    }); */
-  });
+  } catch (e) {
+    console.error("Error reading or parsing JSON file:", e);
+  }
 });
 
+async function sendMessageWithDelay(ctx, comp, delay) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      ctx.telegram.sendMessage(
+        ctx.chat.id,
+        `${comp.title}\n\n${comp.text}\n\n${comp.img}`,
+        {
+          disable_notification: true,
+        }
+      );
+      resolve();
+    }, delay);
+  });
+}
+
 bot.launch();
+
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
